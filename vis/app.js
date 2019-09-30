cf1e14261c9e142676ef38a95df19f4d364d74de
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2019-09-30 00:43:21 -0400

    trying out sellenium and phanantomjs

--- vis/app.js
+++ vis/app.js
@@ -1,3 +1,16 @@
+import { Builder, By, Key, until } from 'selenium-webdriver';
+
+(async function example() {
+  let driver = await new Builder().forBrowser('firefox').build();
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
+
 // Video 1.5
 // document.getElementById("msg").innerText = "Hey, Annika!";
 
@@ -141,14 +154,16 @@
 // drawCircle();
 
 // Video 4.1
-var rotatingSquare = document.getElementById("web-animation").animate(
-    [
-        { backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(0deg) translate3D(0, 0, 0)'},
-        { backgroundColor: '#440000', opacity: 0.3 },
-        { backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(360deg) translate3D(0, 0, 0)'}
-    ], {
-      duration: 500,
-      iterations: Infinity,
-      easing: 'ease-in-out'
-    }
-);
+// var rotatingSquare = document.getElementById("web-animation").animate(
+//     [
+//         { backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(0deg) translate3D(0, 0, 0)'},
+//         { backgroundColor: '#440000', opacity: 0.3 },
+//         { backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(360deg) translate3D(0, 0, 0)'}
+//     ], {
+//       duration: 500,
+//       iterations: Infinity,
+//       easing: 'ease-in-out'
+//     }
+// );
+
+
