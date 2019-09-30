535d0f044e61bb9e43f664ac6563a99071e506cc
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2019-09-30 13:48:31 -0400

    rain and poolinggit add .

---
+++ vis/README.md
@@ -0,0 +1,10 @@
+Khan Acadmey Naturla Simualiton
+https://www.khanacademy.org/computing/computer-programming/programming-natural-simulations
+Two Lynda Tutorials
+
+
+Rain code
+https://www.html5canvastutorials.com/advanced/html5-canvas-rain-effect/
+Rain Tutorial
+https://redstapler.co/three-js-realistic-rain-tutorial/
+ArcGIS
---
+++ vis/Rain Tutorial/app.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    var canvas = $('#canvas')[0];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      var w = canvas.width;
+      var h = canvas.height;
+      var waterHeight = 0;
+      ctx.strokeStyle = 'rgba(174,194,224,0.5)';
+      ctx.lineWidth = 1;
+      ctx.lineCap = 'round';
+  
+      var init = [];
+      var maxParts = 1000;
+      for (var a = 0; a < maxParts; a++) {
+        init.push({
+          x: Math.random() * w,
+          y: Math.random() * h,
+          l: Math.random() * 1,
+          xs: -4 + Math.random() + 2,
+          ys: Math.random() * 2 + 10
+        })
+      }
+  
+      var particles = [];
+      for (var b = 0; b < maxParts; b++) {
+        particles[b] = init[b];
+      }
+  
+      function draw() {
+        ctx.clearRect(0, 0, w, h);
+        for (var c = 0; c < particles.length; c++) {
+          var p = particles[c];
+          ctx.beginPath();
+          ctx.moveTo(p.x, p.y);
+          ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
+          if((p.y + p.l * p.ys)>(h-waterHeight)){
+              pinkSplash(p.x + p.l * p.xs, p.y + p.l * p.ys);
+          }
+          ctx.stroke();
+        }
+        move();
+        increasePool();
+      }
+  
+      function move() {
+        for (var b = 0; b < particles.length; b++) {
+          var p = particles[b];
+          p.x += p.xs;
+          p.y += p.ys;
+          if (p.x > w || p.y > (h-waterHeight)) {
+            p.x = Math.random() * w;
+            p.y = -20;
+          }
+        }
+      }
+
+      function increasePool() {
+        ctx.beginPath();
+        ctx.rect(0, h-waterHeight, w, h);
+        ctx.fillStyle = 'rgba(174,194,224,0.5)';
+        ctx.fill();
+        waterHeight+= 0.25;
+      }
+
+      function pinkSplash(x, y) {
+        ctx.beginPath();
+        var radius = 4;
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = 'pink';
+        ctx.fill();
+      }
+  
+      setInterval(draw, 30);
+  
+    }
+  });
