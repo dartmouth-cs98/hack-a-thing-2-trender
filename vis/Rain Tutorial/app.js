0fed0ae341cf37acb57c15d87d0114f29bb95956
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2019-09-30 17:28:06 -0400

    changed from array to map for efficiency going to get to follow sine wave for bubbles

--- vis/Rain Tutorial/app.js
+++ vis/Rain Tutorial/app.js
@@ -22,53 +22,51 @@ $(document).ready(function() {
           x: Math.random() * w,
           y: Math.random() * h,
           l: Math.random() * 1,
-          xs: -4 + Math.random() + 2,
-          ys: Math.random() * 2 + 10
+          xs: -4 + Math.random() * 4 + 2,
+          ys: Math.random() *10 + 10
         })
       }
   
-      var particles = [];
+      var particles = new Map();
       for (var b = 0; b < maxParts; b++) {
-        particles[b] = init[b];
+        particles.set(init[b].x, init[b]);
       }
   
       function draw() {
         ctx.clearRect(0, 0, w, h);
-        // for (var c = 0; c < particles.length; c++) {
-        //   var p = particles[c];
-        //   ctx.beginPath();
-        //   ctx.moveTo(p.x, p.y);
-        //   ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
-        //   if((p.y + p.l * p.ys)>(h-waterHeight)){
-        //       pinkSplash(p.x + p.l * p.xs, p.y + p.l * p.ys);
-        //   }
-        //   ctx.stroke();
-        // }
-        // move();
+        for (var value of particles.values()) {
+          var p = value;
+          ctx.beginPath();
+          ctx.moveTo(p.x, p.y);
+          ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
+          if((p.y + p.l)>(h-waterHeight)){
+              pinkSplash(p.x + p.l * p.xs, p.y + p.l * p.ys);
+          }
+          ctx.stroke();
+        }
+        move();
         increasePool();
         drawSin();
       }
   
       function move() {
-        for (var b = 0; b < particles.length; b++) {
-          var p = particles[b];
-          p.x += p.xs;
-          p.y += p.ys;
-          if (p.x > w || p.y > (h-waterHeight)) {
+        for (var value of particles.values()) {
+          var p = value;
+          p.x += p.xs*0.3;
+          p.y += p.ys*0.3;
+          if(p.x > w || p.y > h){
             p.x = Math.random() * w;
             p.y = -20;
           }
+          
         }
       }
 
       function drawSin(){
         for(var xCoord = -1*waveMove; xCoord < (-1*waveMove+w); xCoord+=2){
-            if(xCoord%10==0){
-                console.log(xCoord);
-            }
             ctx.beginPath();
             var s = Math.sin(xCoord/20);
-            ctx.rect(xCoord+waveMove, h, 2, (waterHeight/2)+s*20);
+            ctx.rect(xCoord+waveMove, (h-waterHeight)-s*20, 2, h);
             ctx.fillStyle = 'rgba(174,194,224,0.5)';
             ctx.fill();
         }
@@ -76,7 +74,7 @@ $(document).ready(function() {
       }
 
       function increasePool() {
-        waterHeight -= 0.5;
+        waterHeight += 0.5;
       }
 
       function pinkSplash(x, y) {
